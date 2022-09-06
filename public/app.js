786e66a59b10307ba1540fdd900d624ec97413bd
axelhagerback <axel.hagerback@gmail.com>
2022-09-06 12:51:40 +0200

    made the groundwork for the setting and calculator windows

--- public/app.js
+++ public/app.js
@@ -5,17 +5,34 @@ build();
 function build() {
     const targetCalc = document.getElementById('app');
     const divCalc = document.createElement('div');
-    //divCalc.innerHTML = 'Calculator';
-    // För css      title.className = 'red';
-   targetCalc.appendChild(divCalc);
+    divCalc.setAttribute('id', 'calculator');
+    divCalc.className = 'border';
+    targetCalc.appendChild(divCalc);
 
-   const targetSett = document.getElementById('app');
-   const divSett = document.createElement('div');
-    //divSett.innerHTML = 'Settings';
-   targetSett.appendChild(divSett);
+    const targetSett = document.getElementById('app');
+    const divSett = document.createElement('div');
+    divSett.setAttribute('id', 'settings');
+    divSett.className = 'border';
+    targetSett.appendChild(divSett);
 
+    const targetDivCalc = document.getElementById('calculator');
+    const titleCalc = document.createElement('h1');
+    titleCalc.innerHTML = "Calculator";
+    targetDivCalc.appendChild(titleCalc);
+
+    const targetDivSett = document.getElementById('settings');
+    const titleSett = document.createElement('h1');
+    titleSett.innerHTML = "Settings";
+    targetDivSett.appendChild(titleSett);
 }
-    //console.log('sayHello called ...');
+    
+
+
+
+
+
+
+//console.log('sayHello called ...');
    // const contactPersonName = contactPerson.value;
    // console.log(contactPersonName);
     // document.getElementById('namePlaceHolder').innerHTML = contactPersonName;
